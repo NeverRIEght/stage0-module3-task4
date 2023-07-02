651ac31c14bc9daa7b72962c63e3d6fc212d4df5
NeverRIEght <lfkomarovme@gmail.com>
2023-07-02 19:48:44 +0300

    Formula

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.module3.task4;
 
 public class FormulaCreator {
     public static void main(String[] args) {
+        int a, b;
+        a = 1;
+        b = 2;
+
+        double formula = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+
+        System.out.println(formula);
     }
 }
