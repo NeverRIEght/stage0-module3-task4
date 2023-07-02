43bdc3462d6a3dfcca2240b610e3f8f8388f5bf6
NeverRIEght <lfkomarovme@gmail.com>
2023-07-02 20:38:58 +0300

    Fix Formula Checking ChatGPT answer

--- src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
+++ src/main/java/school/mjc/stage0/module3/task4/FormulaCreator.java
@@ -6,8 +6,29 @@ public class FormulaCreator {
         a = 1;
         b = 2;
 
-        String formula = "(9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)";
+//        double formula = (9 * Math.pow(a, 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
 
-        System.out.println(formula);
+//        double formula = (9 * a * a - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2);
+//        double result = ((9 * a * a - 5 * b) + 2 + a - 7) * ((a + b) - (4 * a * b)) / 2;
+
+//        double formula1 = (9 * a * a - 5 * b + 2 + a - 7);
+//        double formula2 = ((a + b - 4 * a * b) / 2);
+//        double result1 = ((9 * a * a - 5 * b) + 2 + a - 7);
+//        double result2 = ((a + b) - (4 * a * b)) / 2;
+//        double result = ((9 * a * a - 5 * b) + 2 + a - 7) * ((a + b) - (4 * a * b)) / 2;
+//
+//        System.out.println(formula1 * formula2);
+//        System.out.println(result1 * result2);
+//        System.out.println(result);
+
+
+
+
+
+        int result = ((9 * a * a - 5 * b) + 2 + a - 7) * ((a + b) - (4 * a * b)) / 2;
+
+        System.out.println(result);
+
+
     }
 }
